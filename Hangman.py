c6d25ca0ac47707bf375d1bb92e1cc6769692b2b
dragon2601 <92962702+dragon2601@users.noreply.github.com>
2023-08-20 17:33:07 -0700

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,29 +1,46 @@
 from cow import CowHangman
 
+def get_word_from_user():
+    #Prompt the user to input a word or phrase for the hangman game.
+    word = input("Enter the word or phrase to be guessed (spaces are allowed): ").upper()
+    while not all(char.isalpha() or char.isspace() for char in word):
+        print("Please only use letters and spaces.")
+        word = input("Enter the word or phrase to be guessed (spaces are allowed): ").upper()
+    return word
+
+
 def play_hangman():
-    word = "Wellman Hall".upper()
+    word = get_word_from_user()
+    print("\n" * 100)  # Clear the console screen to hide the input word
     guessed_letters = []
     display_word = ['_' if letter.isalpha() else letter for letter in word]
     cow_game = CowHangman()
     
-    while cow_game.lives > 0 and '_' in display_word:
-        print(' '.join(display_word))
-        guess = input("Guess a letter: ").upper()
+    current_position = 0
 
-        if guess in guessed_letters:
-            print("You've already guessed that letter!")
+    while cow_game.lives > 0 and current_position < len(word):
+        print(' '.join(display_word))
+        guess = input("Guess the next letter: ").upper()
+
+        # Input validation
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single valid letter.")
             continue
-        
-        if guess in word:
-            for index, letter in enumerate(word):
-                if letter == guess:
-                    display_word[index] = guess
+
+        # Check if the guess matches the current expected letter
+        if guess == word[current_position]:
+            # Update the display word and move to the next position
+            display_word[current_position] = guess
+            current_position += 1
+            
+            # Skip over spaces or non-alphabetic characters
+            while current_position < len(word) and not word[current_position].isalpha():
+                display_word[current_position] = word[current_position]
+                current_position += 1
         else:
             cow_game.lose_life()
-
-        guessed_letters.append(guess)
     
-    if '_' not in display_word:
+    if current_position == len(word):
         print("Congratulations! You've guessed the word:", word)
     else:
         print("Out of lives! The word was:", word)
