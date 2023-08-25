fbb452074e3fdcac99a39a8ee085b7a7f63cd770
JulieKhalilieh <142475162+JulieKhalilieh@users.noreply.github.com>
2023-08-24 17:40:52 -0700

    Create SampleHangman.py

---
+++ SampleHangman.py
@@ -0,0 +1,13 @@
+import random
+#a sample code that guesses based on the most common letters in the category given
+
+words = {"Buildings": ["wellman", "california", "young", "kemper"],
+         "animals": ["cow", "horse", "squirrel", "sheep"],
+         "majors": ["chemistry", "physics", "art", "history"],
+         "events": ["picnic day", "earth day" , "orientation"]}
+
+def common_letter(remaining_letters, category):
+  letter_frequency = {}
+
+  for word in 
+              
