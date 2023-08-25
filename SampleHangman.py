bd6067b3405e3b2abe7ce26e724d3aed5ccbf37d
JulieKhalilieh <142475162+JulieKhalilieh@users.noreply.github.com>
2023-08-24 18:28:56 -0700

    Update SampleHangman.py

--- SampleHangman.py
+++ SampleHangman.py
@@ -7,7 +7,7 @@ import random
 
 
 words = {"Buildings": ["wellman", "california", "young", "kemper"],
-         "animals": ["cow", "horse", "squirrel", "sheep"],
+         "animals": ["cow", "horse", "squirrel", "sheep", "shark"],
          "majors": ["chemistry", "physics", "art", "history"],
          "events": ["picnic day", "earth day", "orientation"]}
 
@@ -19,10 +19,14 @@ def get_words(category):
 
 
 def eliminate_words(length, category):
-    new_words = get_words(category)
-    for word in new_words:
-        if len(word) != length:
-            del new_words[word]
+    new_words = []
+
+    category_words = get_words(category)
+
+    for word in category_words:
+        if len(word) == length:
+            new_words.append(word)
+
     return new_words
 
 
@@ -33,7 +37,9 @@ def nth_common_letter(length, category, letter_number):
 
     for word in possible_words:
         if word[letter_number] in alphabet:
-            letter_frequency[word[letter_number]] = letter_frequency.get(word[letter_number]) + 1
+            letter_frequency[word[letter_number]] = letter_frequency.get(word[letter_number], 0) + 1
+
+    return max(letter_frequency, key = letter_frequency.get)
                   
                   
                   
