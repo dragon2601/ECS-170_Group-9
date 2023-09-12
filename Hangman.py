6148d82b156ba55ffead4f1660c8591b75f65d06
dragon2601 <92962702+dragon2601@users.noreply.github.com>
2023-09-12 12:53:53 +0530

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,75 +1,151 @@
-from cow import CowHangman
-accepted_words = ["WELLMAN HALL"]
+import sys
+import pandas as pd
+import streamlit as st
+import pandas as pd
 
-def get_word_from_user():
-    #Prompt the user to input a word or phrase for the hangman game and check against a database.
+df = pd.read_csv("Database.csv")
+word_database = df['Word'].tolist()
+#word_database = ["WELLMAN HALL", "ARC", "ART", "APT", "APP", "ATV", "YOUNG HALL", "KEMPER HALL", "AGGIES", "GO AGS", "PAVILLION", "eureka", "mathematics", "physics"]
+game_over = False
+
+def compute_letter_weights(word_database):
+    total_letters = 0
+    letter_counts = {char: 0 for char in 'abcdefghijklmnopqrstuvwxyz'}
     
-    def load_words_from_file(filename):
-        #Load words from a file into a set.
-        with open(filename, 'r') as file:
-            # Read the file line by line, strip whitespace, convert to uppercase, and store in a set
-            return {line.strip().upper() for line in file if line.strip()}
+    for word in word_database:
+        word = word.lower()  # Ensure the word is in lowercase for consistency
+        for letter in word:
+            if letter in letter_counts:
+                letter_counts[letter] += 1
+                total_letters += 1
 
-    def add_word_to_file(filename, word):
-        #Append a word to the file.
-        with open(filename, 'a') as file:
-            file.write(f'\n{word}')
-
-    filename = "Database.rtf"
-    accepted_words = load_words_from_file(filename)
+    # Normalize to get frequencies
+    letter_frequencies = {letter: (count / total_letters) * 100 for letter, count in letter_counts.items()}
     
-    word = input("Enter the word or phrase to be guessed (spaces are allowed): ").upper()
+    return letter_frequencies
 
-    # Check if the word is in the accepted words set
-    if word not in accepted_words:
-        choice = input(f"The word '{word}' is not in the accepted list. Would you like to add it? (yes/no): ").lower()
+# Compute the weights at the start of the code
+letter_weights = compute_letter_weights(word_database)
 
-        if choice == "yes":
-            add_word_to_file(filename, word)
-            print(f"'{word}' has been added to the accepted list.")
+class HangmanGame:
+    def __init__(self, word_length):
+        self.word_length = word_length
+        self.state = "_" * word_length
+        self.guesses = []
+
+    def get_state(self):
+        return self.state
+
+    def guess(self, letter):
+        self.guesses.append(letter)
+        if letter in self.state:
+            return True
+        return False
+
+    def update_state(self, positions, letter):
+        state_list = list(self.state)
+        for pos in positions:
+            state_list[pos] = letter
+        self.state = ''.join(state_list)
+
+
+class EntropyBasedPlayer:
+    def __init__(self, word_database):
+        self.word_database = word_database
+        self.already_guessed = []
+
+    def filter_words(self, word_length):
+        return [word for word in self.word_database if len(word) == word_length]
+
+    def next_guess(self, current_state):
+        potential_matches = [word for word in self.filter_words(len(current_state)) if self.matches_state(word, current_state)]
+    
+        # If only one potential word match is left
+        if len(potential_matches) == 1:
+            return potential_matches[0]
+    
+        next_underscore_position = current_state.index("_")
+    
+        frequency_distribution = {}
+        for word in potential_matches:
+            letter = word[next_underscore_position]
+            if letter not in self.already_guessed:
+                frequency_distribution[letter] = frequency_distribution.get(letter, 0) + 1
+
+        if not frequency_distribution:
+            return None
+
+        # Prioritize based on count first, and then by letter weight in case of a tie.
+        guess = max(frequency_distribution, key=lambda k: (frequency_distribution[k], letter_weights.get(k, 0)))
+        self.already_guessed.append(guess)
+        return guess
+    
+
+    def matches_state(self, word, state):
+        for w, s in zip(word, state):
+            if s != '_' and w != s:
+                return False
+        return True
+
+    def reset_guessed(self):
+        self.already_guessed = []
+
+def main():
+    st.title('Hangman Game with AI Opponent')
+
+    # Initialize the session state variables if not already initialized
+    if 'init' not in st.session_state:
+        st.session_state.init = True
+        st.session_state.game_over = False
+        st.session_state.word_length = 5
+        st.session_state.hangman = None
+        st.session_state.player = None
+
+    # Start a new game or change word length
+    if st.session_state.game_over or st.session_state.hangman is None:
+        st.session_state.word_length = st.slider("Enter the length of the word:", 1, 20, st.session_state.word_length)
+        if st.button("Start Game"):
+            st.session_state.hangman = HangmanGame(st.session_state.word_length)
+            st.session_state.player = EntropyBasedPlayer(word_database)
+            st.session_state.game_over = False
+
+    # If game is ongoing
+    if st.session_state.hangman:
+        st.write("Current state:", st.session_state.hangman.get_state())
+        guess = st.session_state.player.next_guess(st.session_state.hangman.get_state())
+
+        if guess is None:
+            st.write("AI is out of guesses!")
+            st.session_state.game_over = True
+            return
+
+        if len(guess) == st.session_state.word_length:
+            is_word = st.radio(f"Is the word {guess}?", ["Yes", "No"])
+            if is_word == "Yes":
+                st.write("AI won!")
+                st.session_state.game_over = True
+            else:
+                st.write("AI lost!")
+                st.session_state.game_over = True
+            return
+
+        st.write("AI guesses:", guess)
+        is_correct = st.radio("Is the guess correct?", ["Yes", "No"])
+        if is_correct == 'Yes':
+            positions = [i for i, char in enumerate(st.session_state.hangman.get_state()) if char == "_"]
+            if positions:
+                st.session_state.hangman.update_state([positions[0]], guess)
+                st.session_state.player.reset_guessed()
         else:
-            print("Please choose a word from the accepted list or add a new word.")
-            return get_word_from_user()  # Recursive call to prompt the user again
+            st.write("Guess is incorrect.")
+            # Assuming cow_game is similar to Hangman game mechanism.
+            # If you have a specific way of handling this with the cow_game logic, you can replace the below lines.
+            if "_" not in st.session_state.hangman.get_state():
+                st.write("AI lost!")
+                st.session_state.game_over = True
+try:
+    if __name__ == "__main__":
+        main()
+except SystemExit:
+    pass
     
-    return word
-
-
-
-
-def play_hangman():
-    word = get_word_from_user()
-    print("\n" * 100)  # Clear the console screen to hide the input word
-    guessed_letters = []
-    display_word = ['_' if letter.isalpha() else letter for letter in word]
-    cow_game = CowHangman()
-    
-    current_position = 0
-
-    while cow_game.lives > 0 and current_position < len(word):
-        print(' '.join(display_word))
-        guess = input("Guess the next letter: ").upper()
-
-        # Input validation
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single valid letter.")
-            continue
-
-        # Check if the guess matches the current expected letter
-        if guess == word[current_position]:
-            # Update the display word and move to the next position
-            display_word[current_position] = guess
-            current_position += 1
-            
-            # Skip over spaces or non-alphabetic characters
-            while current_position < len(word) and not word[current_position].isalpha():
-                display_word[current_position] = word[current_position]
-                current_position += 1
-        else:
-            cow_game.lose_life()
-    
-    if current_position == len(word):
-        print("Congratulations! You've guessed the word:", word)
-    else:
-        print("Out of lives! The word was:", word)
-
-play_hangman()
